8990b63e76fbfedca504110513b904cdcc8b07f2
Naresh Chaurasia <nareshchaurasia@Nareshs-MacBook-Pro.local>
2022-07-29 11:05:20 +0530

    Macbook Pro, Checkin Timestamp::

--- git_home_mac.sh
+++ git_home_mac.sh
@@ -6,6 +6,6 @@ var sys_date
 
 echo date >> sys_date
 
-git commit -m "Macbook Pro, Checkin Timestamp::{sys_date}"
+git commit -m "Macbook Pro, Checkin Timestamp::$sys_date"
 
 git push origin master
